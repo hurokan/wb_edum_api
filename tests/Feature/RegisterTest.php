1aed517695bc5313c87b115a35df9991a0f5813b
Hasan Uddin Rokan <hurokan91@gmail.com>
2020-10-23 21:18:50 +0600

    swagger implementaion

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testsRegistersSuccessfully()
+    {
+        $payload = [
+            'name' => 'John',
+            'email' => 'john@toptal.com',
+            'password' => '123',
+            'password_confirmation' => '123',
+        ];
+
+        $this->json('post', '/api/register', $payload)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'data' => [
+                    'id',
+                    'name',
+                    'email',
+                    'created_at',
+                    'updated_at',
+                    'api_token',
+                ],
+            ]);
+    }
+
+    public function testsRequiresPasswordEmailAndName()
+    {
+        $this->json('post', '/api/register')
+            ->assertStatus(422)
+            ->assertJson([
+                'name' => ['The name field is required.'],
+                'email' => ['The email field is required.'],
+                'password' => ['The password field is required.'],
+            ]);
+    }
+
+    public function testsRequirePasswordConfirmation()
+    {
+        $payload = [
+            'name' => 'John',
+            'email' => 'john@toptal.com',
+            'password' => 'toptal123',
+        ];
+
+        $this->json('post', '/api/register', $payload)
+            ->assertStatus(422)
+            ->assertJson([
+                'password' => ['The password confirmation does not match.'],
+            ]);
+    }
+
+    /**
+     *
+     */
+    public function testUserIsLoggedOutProperly()
+    {
+        $user = factory(User::class)->create(['email' => 'user@test.com']);
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        $this->json('get', '/api/articles', [], $headers)->assertStatus(200);
+        $this->json('post', '/api/logout', [], $headers)->assertStatus(200);
+
+        $user = User::find($user->id);
+
+        $this->assertEquals(null, $user->api_token);
+    }
+
+    public function testUserWithNullToken()
+    {
+        // Simulating login
+        $user = factory(User::class)->create(['email' => 'user@test.com']);
+        $token = $user->generateToken();
+        $headers = ['Authorization' => "Bearer $token"];
+
+        // Simulating logout
+        $user->api_token = null;
+        $user->save();
+
+        $this->json('get', '/api/articles', [], $headers)->assertStatus(401);
+    }
+}
